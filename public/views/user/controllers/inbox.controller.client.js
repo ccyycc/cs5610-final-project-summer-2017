08aabd7cb907321062a4b169f105140e9bc1b627
Chenwei Yin <yin.chenw@husky.neu.edu>
2017-06-28 11:57:20 -0400

    small change

--- public/views/user/controllers/inbox.controller.client.js
+++ public/views/user/controllers/inbox.controller.client.js
@@ -7,7 +7,7 @@
 
         var model = this;
 
-        model.sectionTitle = "Login";
+        model.sectionTitle = "Inbox";
         model.userId = currentUser._id;
 
         // model.renderMessage = renderMessage;
